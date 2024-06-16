e80d404b53ec927e841fbddd16b1b6997a4ac862
manageit <manageit@LT10-90WFLG3>
2024-06-16 12:10:40 +0530

    commit design pattern project

---
+++ Design Pattern/src/patterns/DesignPatternMain.java
@@ -0,0 +1,11 @@
+package patterns;
+
+public class DesignPatternMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Let's start design pattern...");
+
+	}
+
+}
